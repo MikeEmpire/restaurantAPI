0ee99bb58c25014dc71ab9e49e5eb87138197f7a
puchk <kpuchalski@gmail.com>
2017-04-22 11:48:26 -0700

    Starting initial routes

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+
+var reservationArray = [];
+var waitingArray =[];
+
+
+// =============ROUTE=============
+app.get('/' function (req, res) {
+	res.sendFile(path.join(__dirname + 'index.html'))
+});
