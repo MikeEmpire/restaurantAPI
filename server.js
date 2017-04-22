b9f66fbc846d0bcdbac32e7f6953eb7c2b4cc60d
puchk <kpuchalski@gmail.com>
2017-04-22 12:26:23 -0700

    Added api routes

--- server.js
+++ server.js
@@ -16,7 +16,42 @@ var reservationArray = [];
 var waitingArray =[];
 
 
-// =============ROUTE=============
-app.get('/' function (req, res) {
-	res.sendFile(path.join(__dirname + 'index.html'))
+// =============ROUTES=============
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.get('/tables', function (req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'))
+});
+
+app.get('/reserve', function (req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'))
+});
+
+// =============API ROUTES=============
+app.get('/api/tables', function (req, res) {
+	res.json(reservationArray);
+});
+
+app.get('/api/reserve', function (req, res) {
+	res.json(waitingArray);
+});
+
+app.post('/api/tables', function (req, res) {
+	if (tableData.length < 5) {
+		reservationArray.push(req.body);
+	}
+	else {
+		waitingArray.push(req.body);
+	}
+});
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
